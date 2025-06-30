3daaa79756be1c5344c4e316611ebf9333153f12
Im-Risa <ultra.scire@gmail.com>
2025-06-30 19:55:01 +0300

    Создает и подключает файл с функциями из дз 2.33.

---
+++ js/functions.js
@@ -0,0 +1,45 @@
+//1)Функция для проверки длины строки.
+
+let checkingLengthString = (string, maxLength) => {
+    let result = (string.length <= maxLength);
+    return result;
+};
+
+checkLengthString('sjdhfjwu7', 10);
+
+//2)Функция для проверки, является ли строка палиндромом.
+
+let checkingPalindrome = (initialString) => {
+
+let preparedString = initialString.replaceAll(/\s/g, "").toLowerCase();
+
+let reverseString = "";
+
+for (let i = preparedString.length  - 1; i >= 0; i--) {
+reverseString += preparedString[i];
+}
+
+return preparedString === reverseString;
+};
+
+//2.1)Оптимизированная версия.
+
+let checkingPalindrome = (initialString) => {
+let preparedString = initialString.replaceAll(/\s/g, "").toLowerCase();
+let reverseString = preparedString.split("").reverse().join("");
+return preparedString === reverseString;
+};
+
+//3)
+
+let numberSearch = (entry) => {
+let string = String(entry);
+return parseInt(string.replaceAll(/\D/g, ""));
+}
+
+//3.1)
+
+let numberSearch = (entry) => {
+let string = (typeof entry === 'string') ? entry : String(entry);
+return parseInt(string.replaceAll(/\D/g, ""));
+}
