0531005a577c72bce9d235c3d58fc97f4314687a
Im-Risa <ultra.scire@gmail.com>
2025-07-03 19:00:53 +0300

    Удаляет дополнительные варианты функций

--- js/functions.js
+++ js/functions.js
@@ -1,44 +1,20 @@
 //1)Функция для проверки длины строки.
 
-const checkingLengthString = (string, maxLength) => {
-    return (string.length <= maxLength);
-};
+const checkingLengthString = (string, maxLength) => (string.length <= maxLength);
 
 checkingLengthString('sjdhfjwu7', 10);
 
 //2)Функция для проверки, является ли строка палиндромом.
 
 const checkingPalindrome = (initialString) => {
-
-const preparedString = initialString.replaceAll(/\s/g, "").toLowerCase();
-
-let reverseString = "";
-
-for (let i = preparedString.length  - 1; i >= 0; i--) {
-reverseString += preparedString[i];
-}
-
-return preparedString === reverseString;
-};
-
-//2.1)Оптимизированная версия.
-
-const checkingPalindrome = (initialString) => {
-const preparedString = initialString.replaceAll(/\s/g, "").toLowerCase();
-const reverseString = preparedString.split("").reverse().join("");
-return preparedString === reverseString;
+  const preparedString = initialString.replaceAll(/\s/g, '').toLowerCase();
+  const reverseString = preparedString.split('').reverse().join('');
+  return preparedString === reverseString;
 };
 
 //3)
 
 const numberSearch = (entry) => {
-const string = String(entry);
-return parseInt(string.replaceAll(/\D/g, ""));
-}
-
-//3.1)
-
-const numberSearch = (entry) => {
-const string = (typeof entry === 'string') ? entry : String(entry);
-return parseInt(string.replaceAll(/\D/g, ""));
-}
+  const string = String(entry);
+  return parseInt(string.replaceAll(/\D/g, ''));
+};
